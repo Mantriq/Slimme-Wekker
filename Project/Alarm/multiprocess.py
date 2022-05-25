654e7aac2b1172bbed6e933d29d8f8c429859394
Mantriq <100362818+Mantriq@users.noreply.github.com>
2022-05-25 12:53:30 +0200

    Delete Project directory

--- Project/Alarm/multiprocess.py
+++
@@ -1,7 +0,0 @@
-import multiprocessing
-from playsound import playsound
-
-p = multiprocessing.Process(target=playsound, args=("/Programma_Alarm_morning_alarm.mp3",))
-p.start()
-input("press ENTER to stop playback")
-p.terminate()
